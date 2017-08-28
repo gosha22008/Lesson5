f4df8f6f280edcc31d1a61192682cef1797db244
gosha22008 <gosha22008@yandex.ru>
2017-08-28 14:37:24 +0300

    усложненное переделанное

--- index.php
+++ index.php
@@ -14,21 +14,20 @@ $data = json_decode($content, true);
         <td>Имя</td>
         <td>Фамилия</td>
         <td>Адрес</td>
-        <td>Телефон</td>
+        <td>Телефоны</td>
     </tr>
-        <tr>
-            <td><?= $data[firstName] ?></td>
-            <td><?= $data[secondName] ?></td>
-            <td><?php foreach ($data['address'] as $key => $val) {
-                echo $data["address"]["$key"]." ";
-                } ?>
-            </td>
-            <td>
-                <?php foreach ($data['phoneNumbers'] as $v) {
-                    echo $v." ";
-                } ?>
-            </td>
-        </tr>
+    <tr>
+        <td><?= $data['firstName'] ?></td>
+        <td><?= $data['secondName'] ?></td>
+        <td>
+            <?= $data['address']['city'] . ", " . $data['address']['street'] . ", " . $data['address']['house'] ?>
+        </td>
+        <td>
+            <?php foreach ($data['phoneNumbers'] as $v) {
+                echo $v . " ";
+            } ?>
+        </td>
+    </tr>
 
 </table>
 </body>
